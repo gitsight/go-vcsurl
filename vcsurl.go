932878f8111b4032a1def1767d09b692059dd728
Máximo Cuadros <mcuadros@gmail.com>
2020-05-09 17:12:10 +0200

    major refactoring

--- vcsurl.go
+++ vcsurl.go
@@ -1,6 +1,7 @@
 package vcsurl
 
 import (
+	"errors"
 	"fmt"
 	"net/url"
 	"path/filepath"
@@ -8,26 +9,44 @@ import (
 	"strings"
 )
 
+var (
+	ErrUnknownURL = errors.New("unknown URL format")
+)
+
+// Host VCS provider.
 type Host string
 
+// Supported VCS host provider.
 const (
-	GitHub     Host = "github.com"
-	Bitbucket  Host = "bitbucket.org"
-	GoogleCode Host = "code.google.com"
-	PythonOrg  Host = "hg.python.org"
-	Launchpad  Host = "launchpad.net"
-	GitLab     Host = "gitlab.com"
+	GitHub    Host = "github.com"
+	Bitbucket Host = "bitbucket.org"
+	GitLab    Host = "gitlab.com"
 
 	gitHubAPI Host = "api.github.com"
 )
 
+// Kind of VCS
 type Kind string
 
+// Supported VCS kinds.
 const (
-	Git       Kind = "git"
-	Mercurial Kind = "hg"
-	Bazaar    Kind = "bzr"
+	Git Kind = "git"
 )
+
+// Protocol of remote
+type Protocol string
+
+// Supported VCS protocols.
+const (
+	SSH   Protocol = "ssh"
+	HTTPS Protocol = "https"
+)
+
+var kindByHost = map[Host]Kind{
+	GitHub:    Git,
+	gitHubAPI: Git,
+	GitLab:    Git,
+}
 
 // VCS describes a VCS repository.
 type VCS struct {
@@ -35,8 +54,8 @@ type VCS struct {
 	ID string
 	// CloneURL git remote format.
 	CloneURL string
-	// VCS type
-	VCS Kind
+	// Kind of VCS.
+	Kind Kind
 	// Host is the public web of the repository.
 	Host Host
 	// Username of repo owner on repo hosting site.
@@ -49,140 +68,132 @@ type VCS struct {
 	Rev string
 }
 
-// Link returns the URL to the repository that is intended for access by humans
-// using a Web browser (i.e., not the URL to the API resource).
-func (r *VCS) Link() string {
-	switch r.Host {
-	case GoogleCode:
-		return fmt.Sprintf("https://code.google.com/p/%s", r.FullName)
-	default:
-		return (&url.URL{Scheme: "https", Host: string(r.Host), Path: "/" + r.FullName}).String()
-	}
-}
-
 var (
 	removeDotGit    = regexp.MustCompile(`\.git$`)
 	gitPreprocessRE = regexp.MustCompile("^git@([a-zA-Z0-9-_\\.]+)\\:(.*)$")
 )
 
-// Parses a string that resembles a VCS repository URL. See TestParse for a list of supported URL
-// formats.
-func Parse(spec string) (info *VCS, err error) {
+// Parse parses a string that resembles a VCS repository URL. See TestParse for
+// a list of supported URL formats.
+func Parse(spec string) (*VCS, error) {
 	if parts := gitPreprocessRE.FindStringSubmatch(spec); len(parts) == 3 {
 		spec = fmt.Sprintf("git://%s/%s", parts[1], parts[2])
 	}
 
-	var parsedURL *url.URL
-	if parsedURL, err = url.Parse(spec); err == nil {
-		if parsedURL.Scheme == "" {
-			spec = "https://" + spec
-			if parsedURL, err = url.Parse(spec); err != nil {
-				return nil, err
-			}
-		}
-
-		info = new(VCS)
-
-		info.CloneURL = parsedURL.String()
-		info.Host = Host(parsedURL.Host)
-		info.Rev = parsedURL.Fragment
-
-		if info.Host == GitHub || parsedURL.Scheme == "git" {
-			info.VCS = Git
-		} else if info.Host == GoogleCode && parsedURL.Scheme == "https" {
-			info.VCS = Mercurial
-		} else if info.Host == Bitbucket && (parsedURL.Scheme == "https" || parsedURL.Scheme == "http") {
-			if !strings.HasSuffix(parsedURL.Path, ".git") {
-				info.VCS = Mercurial
-			}
-		} else if info.Host == Launchpad {
-			info.VCS = Bazaar
-		}
-
-		path := parsedURL.Path
-		switch info.Host {
-		case GitHub:
-			parts := strings.Split(path, "/")
-			if len(parts) >= 3 {
-				info.Username = parts[1]
-				info.Name = removeDotGit.ReplaceAllLiteralString(parts[2], "")
-				info.FullName = info.Username + "/" + info.Name
-				info.CloneURL = "git://github.com/" + info.FullName + ".git"
-			}
-		case gitHubAPI:
-			parts := strings.Split(path, "/")
-			if len(parts) >= 4 && parts[1] == "repos" {
-				info.VCS = Git
-				info.Host = GitHub
-				info.Username = parts[2]
-				info.Name = removeDotGit.ReplaceAllLiteralString(parts[3], "")
-				info.FullName = info.Username + "/" + info.Name
-				info.CloneURL = "git://github.com/" + info.FullName + ".git"
-			}
-
-			if len(parts) >= 6 && parts[4] == "commits" {
-				info.Rev = parts[5]
-			}
-
-		case GoogleCode:
-			parts := strings.Split(path, "/")
-			if len(parts) >= 3 && parts[1] == "p" {
-				info.Name = parts[2]
-				info.FullName = info.Name
-				info.CloneURL = "https://code.google.com/p/" + info.FullName
-			}
-		case PythonOrg:
-			parts := strings.Split(path, "/")
-			if len(parts) >= 2 {
-				info.CloneURL = "http://hg.python.org" + path
-				info.VCS = Mercurial
-				info.Name = parts[len(parts)-1]
-				info.FullName = strings.Join(parts[1:], "/")
-			}
-		case Bitbucket:
-			parts := strings.Split(path, "/")
-			if len(parts) >= 3 {
-				info.Username = parts[1]
-				if strings.HasSuffix(parts[2], ".git") {
-					info.VCS = Git
-					parts[2] = strings.TrimSuffix(parts[2], ".git")
-				}
-				info.Name = parts[2]
-				info.FullName = parts[1] + "/" + parts[2]
-				info.CloneURL = "https://bitbucket.org/" + info.FullName
-				if info.VCS == Git {
-					info.CloneURL += ".git"
-				}
-			}
-		default:
-			if len(path) == 0 {
-				return nil, fmt.Errorf("empty path in repo spec: %q", spec)
-			}
-			path = path[1:] // remove leading slash
-			path = removeDotGit.ReplaceAllLiteralString(path, "")
-			info.FullName = path
-			info.Name = filepath.Base(path)
-			if strings.Contains(spec, "git") {
-				info.VCS = Git
-			} else if strings.Contains(spec, "hg") || strings.Contains(spec, "mercurial") {
-				info.VCS = Mercurial
-			}
-		}
-
-		if info.Host == Launchpad {
-			parsedURL.Scheme = "bzr"
-			info.CloneURL = parsedURL.String()
-		}
-
-		if info.Name == "" || info.FullName == "" {
-			return nil, fmt.Errorf("unable to determine name or full name for repo spec %q", spec)
-		}
-
-		if info.ID == "" {
-			info.ID = fmt.Sprintf("%s/%s", string(info.Host), info.FullName)
-		}
-
-		return info, nil
+	parsedURL, err := url.Parse(spec)
+	if err != nil {
+		return nil, err
 	}
-	return nil, err
+
+	if parsedURL.Scheme == "" {
+		spec = "https://" + spec
+		if parsedURL, err = url.Parse(spec); err != nil {
+			return nil, err
+		}
+	}
+
+	info := &VCS{}
+
+	info.CloneURL = parsedURL.String()
+	info.Host = Host(parsedURL.Host)
+	info.Rev = parsedURL.Fragment
+	info.Kind = kindByHost[info.Host]
+
+	path := parsedURL.Path
+	switch info.Host {
+	case GitHub, gitHubAPI:
+		if err := info.parseGitHub(parsedURL); err != nil {
+			return nil, err
+		}
+
+	case Bitbucket:
+		parts := strings.Split(path, "/")
+		if len(parts) >= 3 {
+			info.Username = parts[1]
+			if strings.HasSuffix(parts[2], ".git") {
+				info.Kind = Git
+				parts[2] = strings.TrimSuffix(parts[2], ".git")
+			}
+			info.Name = parts[2]
+			info.FullName = parts[1] + "/" + parts[2]
+		}
+	default:
+		if len(path) == 0 {
+			return nil, fmt.Errorf("empty path in repo spec: %q", spec)
+		}
+		path = path[1:] // remove leading slash
+		path = removeDotGit.ReplaceAllLiteralString(path, "")
+		info.FullName = path
+		info.Name = filepath.Base(path)
+		if strings.Contains(spec, "git") {
+			info.Kind = Git
+		}
+	}
+
+	if info.Name == "" || info.FullName == "" {
+		return nil, fmt.Errorf("unable to determine name or full name for repo spec %q", spec)
+	}
+
+	if info.ID == "" {
+		info.ID = fmt.Sprintf("%s/%s", string(info.Host), info.FullName)
+	}
+
+	return info, nil
+
+}
+
+func (v *VCS) parseGitHub(url *url.URL) error {
+	parts := strings.Split(url.Path, "/")
+	if v.Host == gitHubAPI {
+		v.Host = GitHub
+		if len(parts) < 2 || parts[1] != "repos" {
+			return ErrUnknownURL
+		}
+
+		parts = parts[1:]
+	}
+
+	if len(parts) < 3 {
+		return ErrUnknownURL
+	}
+
+	v.Username = parts[1]
+	v.Name = removeDotGit.ReplaceAllLiteralString(parts[2], "")
+	v.FullName = v.Username + "/" + v.Name
+
+	if len(parts) >= 5 && (parts[3] == "commits" || parts[3] == "commit") {
+		v.Rev = parts[4]
+	}
+
+	return nil
+}
+
+func (v *VCS) Remote(p Protocol) string {
+	switch p {
+	case SSH:
+		return v.sshRemote()
+	case HTTPS:
+		return v.httpsRemote()
+	default:
+		return ""
+	}
+}
+
+// git@gitlab.com:commento/docs.git
+// git@github.com:go-git/go-git.git
+// git clone git@bitbucket.org:mcuadros/discovery-rest.git
+func (v *VCS) sshRemote() string {
+	return fmt.Sprintf("git@%s/%s/%s.git", v.Host, v.Username, v.Name)
+}
+
+// https://mcuadros@bitbucket.org/mcuadros/discovery-rest.git
+// https://gitlab.com/commento/docs.git
+// https://github.com/go-git/go-git.git
+func (v *VCS) httpsRemote() string {
+	var auth string
+	if v.Host == Bitbucket {
+		auth = fmt.Sprintf("%s@", v.Username)
+	}
+
+	return fmt.Sprintf("https://%s%s/%s/%s.git", auth, v.Host, v.Username, v.Name)
 }
--- vcsurl_test.go
+++ vcsurl_test.go
@@ -1,171 +1,51 @@
-package vcsurl
+package vcsurl_test
 
 import (
 	"testing"
 
-	"github.com/kr/pretty"
+	"github.com/gitsight/go-vcsurl"
+	"github.com/stretchr/testify/require"
 )
 
-var (
-	githubUserRepo = VCS{
-		ID:       "github.com/user/repo",
-		CloneURL: "git://github.com/user/repo.git",
-		VCS:      Git,
-		Host:     GitHub,
-		Username: "user",
-		Name:     "repo",
-		FullName: "user/repo",
-		Rev:      "asdf",
-	}
-	googleCodeRepo = VCS{
-		ID:       "code.google.com/go",
-		CloneURL: "https://code.google.com/p/go",
-		VCS:      Mercurial,
-		Host:     GoogleCode,
-		Name:     "go",
-		FullName: "go",
-	}
-	cpythonRepo = VCS{
-		ID:       "hg.python.org/cpython",
-		CloneURL: "http://hg.python.org/cpython",
-		VCS:      Mercurial,
-		Host:     PythonOrg,
-		Name:     "cpython",
-		FullName: "cpython",
-	}
-	bitbucketHgRepo = VCS{
-		ID:       "bitbucket.org/user/repo",
-		CloneURL: "https://bitbucket.org/user/repo",
-		VCS:      Mercurial,
-		Host:     Bitbucket,
-		Username: "user",
-		Name:     "repo",
-		FullName: "user/repo",
-	}
-	bitbucketGitRepo = VCS{
-		ID:       "bitbucket.org/user/repo",
-		CloneURL: "https://bitbucket.org/user/repo.git",
-		VCS:      Git,
-		Host:     Bitbucket,
-		Username: "user",
-		Name:     "repo",
-		FullName: "user/repo",
-	}
-	launchpadRepo = VCS{
-		ID:       "launchpad.net/repo",
-		CloneURL: "bzr://launchpad.net/repo",
-		VCS:      Bazaar,
-		Host:     Launchpad,
-		Username: "",
-		Name:     "repo",
-		FullName: "repo",
-	}
-)
-
-func TestParse(t *testing.T) {
-	tests := []struct {
-		url  string
-		rid  string
-		info VCS
-	}{
-		{"github.com/user/repo#asdf", "github.com/user/repo", githubUserRepo},
-		{"http://github.com/user/repo#asdf", "github.com/user/repo", githubUserRepo},
-		{"http://github.com/user/repo.git#asdf", "github.com/user/repo", githubUserRepo},
-		{"https://github.com/user/repo#asdf", "github.com/user/repo", githubUserRepo},
-		{"https://github.com/user/repo.git#asdf", "github.com/user/repo", githubUserRepo},
-		{"git://github.com/user/repo#asdf", "github.com/user/repo", githubUserRepo},
-		{"git://github.com/user/repo.git#asdf", "github.com/user/repo", githubUserRepo},
-		{"git+ssh://github.com/user/repo#asdf", "github.com/user/repo", githubUserRepo},
-		{"git+ssh://github.com/user/repo.git#asdf", "github.com/user/repo", githubUserRepo},
-		{"git@github.com:user/repo#asdf", "github.com/user/repo", githubUserRepo},
-		{"git@github.com:user/repo.git#asdf", "github.com/user/repo", githubUserRepo},
-		{"https://api.github.com/repos/user/repo/commits/asdf", "github.com/user/repo", githubUserRepo},
-
-		{"code.google.com/p/go", "code.google.com/p/go", googleCodeRepo},
-		{"https://code.google.com/p/go", "code.google.com/p/go", googleCodeRepo},
-
-		{"hg.python.org/cpython", "hg.python.org/cpython", cpythonRepo},
-		{"http://hg.python.org/cpython", "hg.python.org/cpython", cpythonRepo},
-
-		{"bitbucket.org/user/repo", "bitbucket.org/user/repo", bitbucketHgRepo},
-		{"https://bitbucket.org/user/repo", "bitbucket.org/user/repo", bitbucketHgRepo},
-		{"http://bitbucket.org/user/repo", "bitbucket.org/user/repo", bitbucketHgRepo},
-
-		{"bitbucket.org/user/repo.git", "bitbucket.org/user/repo", bitbucketGitRepo},
-		{"https://bitbucket.org/user/repo.git", "bitbucket.org/user/repo", bitbucketGitRepo},
-		{"http://bitbucket.org/user/repo.git", "bitbucket.org/user/repo", bitbucketGitRepo},
-
-		{"http://launchpad.net/repo", "launchpad.net/repo", launchpadRepo},
-		{"bzr://launchpad.net/repo", "launchpad.net/repo", launchpadRepo},
-		{"bzr+ssh://launchpad.net/repo", "launchpad.net/repo", launchpadRepo},
-
-		// subpaths
-		{"http://github.com/user/repo/subpath#asdf", "github.com/user/repo", githubUserRepo},
-		{"git@github.com:user/repo.git/subpath#asdf", "github.com/user/repo", githubUserRepo},
-		{"https://code.google.com/p/go/subpath", "code.google.com/p/go", googleCodeRepo},
-
-		// other repo hosts
-		{"git://example.com/foo", "example.com/foo", VCS{
-			ID:       "example.com/foo",
-			CloneURL: "git://example.com/foo",
-			VCS:      Git,
-			Host:     "example.com",
-			Name:     "foo",
-			FullName: "foo",
-		}},
-		{"https://example.com/foo.git", "example.com/foo", VCS{
-			ID:       "example.com/foo",
-			CloneURL: "https://example.com/foo.git",
-			VCS:      Git,
-			Host:     "example.com",
-			Name:     "foo",
-			FullName: "foo",
-		}},
-		{"https://example.com/git/foo", "example.com/foo", VCS{
-			ID:       "example.com/git/foo",
-			CloneURL: "https://example.com/git/foo",
-			VCS:      Git,
-			Host:     "example.com",
-			Name:     "foo",
-			FullName: "git/foo",
-		}},
-		{"git@git.private.com:org/repo.git", "git.private.com/org/repo", VCS{
-			ID:       "git.private.com/org/repo",
-			CloneURL: "git://git.private.com/org/repo.git",
-			VCS:      Git,
-			Host:     "git.private.com",
-			Name:     "repo",
-			FullName: "org/repo",
-		}},
+func TestParse_GitHub(t *testing.T) {
+	urls := []string{
+		"github.com/foo/bar",
+		"http://github.com/foo/bar",
+		"https://github.com/foo/bar",
+		"https://github.com/foo/bar.git",
+		"https://api.github.com/repos/foo/bar",
+		"git@github.com:foo/bar",
+		"git@github.com:foo/bar.git",
+		"git+ssh://github.com/foo/bar",
 	}
 
-	for _, test := range tests {
-		info, err := Parse(test.url)
-		if err != nil {
-			t.Errorf("clone URL %q: got error: %s", test.url, err)
-			continue
-		}
-		if test.info != *info {
-			t.Errorf("%s: %v", test.url, pretty.Diff(test.info, *info))
-		}
+	for _, url := range urls {
+		vcs, err := vcsurl.Parse(url)
+		require.NoError(t, err)
+		AssertVCS_GitHub(t, vcs)
 	}
 }
 
-func TestLink(t *testing.T) {
-	tests := []struct {
-		repo VCS
-		link string
-	}{
-		{githubUserRepo, "https://github.com/user/repo"},
-		{bitbucketHgRepo, "https://bitbucket.org/user/repo"},
-		{bitbucketGitRepo, "https://bitbucket.org/user/repo"},
-		{googleCodeRepo, "https://code.google.com/p/go"},
+func TestParse_GitHubRevision(t *testing.T) {
+	urls := []string{
+		"github.com/foo/bar#qux",
+		"https://github.com/foo/bar/commit/qux",
+		"https://api.github.com/repos/foo/bar/commits/qux",
 	}
 
-	for _, test := range tests {
-		link := test.repo.Link()
-		if test.link != link {
-			t.Errorf("%s: want link %q, got %q", test.repo.CloneURL, test.link, link)
-		}
+	for _, url := range urls {
+		vcs, err := vcsurl.Parse(url)
+		require.NoError(t, err)
+		require.Equal(t, vcs.Rev, "qux")
+		AssertVCS_GitHub(t, vcs)
 	}
+}
+
+func AssertVCS_GitHub(t *testing.T, vcs *vcsurl.VCS) {
+	t.Helper()
+	require.Equal(t, vcs.Kind, vcsurl.Git)
+	require.Equal(t, vcs.Host, vcsurl.GitHub)
+	require.Equal(t, vcs.Username, "foo")
+	require.Equal(t, vcs.Name, "bar")
+	require.Equal(t, vcs.FullName, "foo/bar")
 }
